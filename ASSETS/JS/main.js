70e35f60c756d8b871797ed519f685bd7f236a7b
Lorenzo-Calzi <lorenzocalzi@gmail.com>
2021-04-26 16:40:04 +0200

    Correzioni generali e aggiunta parte estetica

--- ASSETS/JS/main.js
+++ ASSETS/JS/main.js
@@ -1,42 +1,48 @@
 /* EMAIL */
 
 // Chiedi all’utente la sua email
-var email = prompt('Inserisci la tua Email')
-console.log(email);
+var email = prompt('Inserisci la tua Email');
 
-var verifiedEmail = ["email_1@gmail.com", "email_2@gmail.com", "email_3@gmail.com", "email_4@gmail.com", "email_5@gmail.com"]
-console.log(verifiedEmail);
+var userName = prompt('Inserisci il nome con il quale vuoi partecipare');
+document.getElementById('user_name').innerHTML = 'Benevenuto ' + userName;
+
+var verifiedEmail = ["lorenzocalzi@gmail.com", "email_1@gmail.com", "email_2@gmail.com", "email_3@gmail.com", "email_4@gmail.com"];
 
 // Controlla che sia nella lista di chi può accedere
 for (var i = 0; i < verifiedEmail.length; i++) {
     if (verifiedEmail[i] == email){
-        document.getElementById('user_email').innerHTML = 'La tua email è: ' + verifiedEmail[i] + ' ed è presente nel nostro database.'
+        document.getElementById('user_email').innerHTML = verifiedEmail[i] ;
+        alert('Benvenuto!')
         break;
     }
 
     else{
-        alert("L'email inserita non è presente all'interno del database.")
+        alert("ATTENZIONE! Non potrai riscuotere l'eventuale vincita poiché l'email inserita non è presente all'interno del nostro database.");
         break;
     }  
 }
 
+/* /EMAIL */
 
 /* DADI */
 
 //Generare un numero random da 1 a 6 sia per il giocatore sia per il computer
 var userDie = Math.floor(Math.random() * 6) + 1;
-console.log(userDie);
+document.getElementById('user_die').innerHTML = userName + ' ha giocato: ' + userDie;
 
 var computerDie = Math.floor(Math.random() * 6) + 1;
-console.log(computerDie);
+document.getElementById('computer_die').innerHTML = 'Il computer ha giocato: ' + computerDie;
 
 //Stabilire il vincitore, in base a chi fa il punteggio più alto.
 if (userDie > computerDie){
-    console.log("Ha vinto l'utente: " + userDie);
+    document.getElementById('winner').innerHTML = 'Il vincitore è ' + userName;
 
 } else if (userDie < computerDie ){
-    console.log("Ha vinto il computer: " + computerDie);
+    document.getElementById('winner').innerHTML = 'Il vincitore è il Computer';
+
 
 } else if (userDie == computerDie){
-    console.log('Pareggio');
+    document.getElementById('winner').innerHTML = 'La partita è finita in pareggio';
 }
+
+/* /DADI */
